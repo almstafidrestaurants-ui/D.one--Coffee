5a748cd1517a30a61b2670feee7717c166167d4a
almstafidrestaurants-ui <almstafidrestaurants@gmail.com>
2026-06-15 13:25:44 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,72 +1,3 @@
-const buttons = document.querySelectorAll(".category-item");
-
-const cards = document.querySelectorAll(".card");
-
-/* فلترة الأقسام */
-
-buttons.forEach(button => {
-
-  button.addEventListener("click", () => {
-
-    // active button
-    buttons.forEach(btn => btn.classList.remove("active"));
-
-    button.classList.add("active");
-
-    const filter = button.dataset.filter;
-
-    cards.forEach(card => {
-
-      if(card.dataset.category === filter){
-
-        card.style.display = "block";
-
-        setTimeout(() => {
-
-          card.classList.add("show");
-
-        }, 50);
-
-      }
-
-      else{
-
-        card.style.display = "none";
-
-        card.classList.remove("show");
-      }
-
-    });
-
-  });
-
-});
-
-/* عرض قسم hot بالبداية */
-
-cards.forEach(card => {
-
-  if(card.dataset.category === "hot"){
-
-    card.style.display = "block";
-
-    setTimeout(() => {
-
-      card.classList.add("show");
-
-    }, 50);
-
-  }
-
-  else{
-
-    card.style.display = "none";
-  }
-
-});
-
-
-
 
 
 /* =======================================COFFEE MODAL ===========================================*/
@@ -77,5 +8,4 @@ function openModal(id){
 function closeModal(id){
   document.getElementById(id).style.display = "none";
 }
-
 
